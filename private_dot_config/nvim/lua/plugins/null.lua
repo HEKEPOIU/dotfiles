511a9a1873501daa0999d28958dc9b9332575ba8
Hekepoiu <decnovpk92805@gmail.com>
2024-08-13 00:14:11 +0800

    Add none-ls for nvim

---
+++ private_dot_config/nvim/lua/plugins/null.lua
@@ -0,0 +1,72 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "ThePrimeagen/refactoring.nvim",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-treesitter/nvim-treesitter",
+            },
+            config = function()
+                require("refactoring").setup(
+                    {
+                        -- prompt for return type
+                        prompt_func_return_type = {
+                            go = true,
+                            cpp = true,
+                            c = true,
+                            java = true,
+                        },
+                        -- prompt for function parameters
+                        prompt_func_param_type = {
+                            go = true,
+                            cpp = true,
+                            c = true,
+                            java = true,
+                        },
+                    }
+                )
+            end,
+        },
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup {
+            sources = {
+                null_ls.builtins.completion.spell,
+                null_ls.builtins.code_actions.gitsigns,
+                null_ls.builtins.code_actions.refactoring.with({
+                    filetypes = {
+                        "go",
+                        "javascript",
+                        "lua",
+                        "python",
+                        "typescript",
+                        "cpp",
+                        "cs",
+                        "csharp"
+                    }
+                }),
+                null_ls.builtins.formatting.prettierd.with({
+                    filetypes = {
+                        "javascript",
+                        "typescript",
+                        "javascriptreact",
+                        "typescriptreact",
+                        "css",
+                        "scss",
+                        "html",
+                        "json",
+                        "yaml",
+                        "markdown",
+                        "graphql",
+                        "md",
+                        "txt",
+                    },
+                    only_local = "node_modules/.bin",
+                }),
+            }
+        }
+    end
+}
