31180fe6ac8a804520495e8a3f9f0ded878e8c65
bojing lin <101374163+HEKEPOIU@users.noreply.github.com>
2024-09-08 07:46:32 +0800

    Update telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,19 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find Files with Telescope" })
 
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>pg', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end, { desc = "Grep for String with Telescope" })
 
         vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "List Open Buffers with Telescope" })
+        vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {desc = "Lists LSP references for word under the cursor"})
+        vim.keymap.set('n', '<leader>psd', builtin.lsp_document_symbols, {desc = "Lists LSP document symbols in the current buffer"})
+        vim.keymap.set('n', '<leader>psw', builtin.lsp_workspace_symbols, {desc = "Lists LSP document symbols in the current workspace"})
+        vim.keymap.set('n', '<leader>psa', builtin.lsp_dynamic_workspace_symbols, {desc = "Dynamically Lists LSP for all workspace symbols"})
+        vim.keymap.set('n', '<leader>pi', builtin.lsp_implementations, {desc = "Goto the implementation of the word under the cursor"})
+
+        vim.keymap.set('n', '<leader>pd', builtin.lsp_definitions, {desc = "Goto the definition of the word under the cursor"})
+        vim.keymap.set('n', '<leader>ptd', builtin.lsp_type_definitions, {desc = "Goto the definition of the type of the word under the cursor"})
+
     end
 }
