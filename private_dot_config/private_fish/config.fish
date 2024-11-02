4db4314f7eb408b9d5a07d27a077c5dd2570d8e2
Hekepoiu <decnovpk92805@gmail.com>
2024-11-02 22:16:44 +0800

    Add Function to open pdf quickly in bash and fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     set FLINE_PATH $HOME/.config/fish/fishline
     source $FLINE_PATH/init.fish
 end
+function open_pdf_in_browser
+    bash -c "source ~/.bashrc && open_pdf_in_browser '$argv[1]'" 
+end
+
 bass source /etc/profile
 bass source ~/.profile
 bass source ~/.bashrc
@@ -9,3 +13,4 @@ neofetch
 nvm use lts
 alias d="kitten diff"
 alias icat="kitten icat"
+alias opib="open_pdf_in_browser"
