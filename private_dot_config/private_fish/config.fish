2fc6ab2bb60a286f05893818195601521bdb3623
Hekepoiu <decnovpk92805@gmail.com>
2025-01-07 12:17:38 +0800

    Add fzt function utils

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,6 +6,24 @@ function open_pdf_in_browser
     bash -c "source ~/.bashrc && open_pdf_in_browser '$argv[1]'" 
 end
 
+# Fish Version of fzt, may some day i will change to zsh so that i dont write two version.
+function fzt
+    set p (fzf)
+    if test -z "$p"
+        echo "No selection made"
+        return 1
+    end
+
+    set dir (dirname "$p")
+    echo $dir
+
+    cd $dir
+    if test $status -ne 0
+        echo "Failed to change directory to $dir"
+        return 1
+    end
+end
+
 bass source /etc/profile
 bass source ~/.profile
 bass source ~/.bashrc
