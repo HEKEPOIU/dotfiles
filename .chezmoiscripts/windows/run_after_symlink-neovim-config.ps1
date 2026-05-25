c1b48f589660e94c2377b4a519b178fbdb8050cf
Hekepoiu <decnovpk92805@gmail.com>
2026-05-26 01:02:04 +0800

    add lazygit config

--- .chezmoiscripts/windows/run_after_symlink-neovim-config.ps1
+++ .chezmoiscripts/windows/run_after_symlink-neovim-config.ps1
@@ -2,4 +2,8 @@ If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
     If (-Not (Test-Path $env:LOCALAPPDATA\nvim)) {
         New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config\nvim
     }
+
+    If (-Not (Test-Path $env:LOCALAPPDATA\lazygit)) {
+        New-Item -Path $env:LOCALAPPDATA\lazygit -ItemType Junction -Value $env:USERPROFILE\.config\lazygit
+    }
 }
